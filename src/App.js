f41dedd7d20980b2e017f6b9cf20f6225434137d
Sreeja R <SreejaRadish@gmail.com>
2021-11-11 17:16:57 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+import data from './components/data';
+import { useState } from 'react';
 
 function App() {
+  const { products } = data;
+  const [cartItems,setCartItems] = useState([]); 
+  const onAdd = (product) => {
+    const exist = cartItems.find(x => x.id === product.id);
+    if(exist){
+setCartItems(cartItems.map((item) =>
+ item.id === product.id ? {...exist,qty: exist.qty+1} : item)
+ );
+    } 
+    else{
+setCartItems([...cartItems, {...product,qty: 1}]);
+    }
+  }
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if(exist.qty === 1){
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    }
+    else{
+      setCartItems(cartItems.map((item) =>
+      item.id === product.id ? {...exist,qty: exist.qty-1} : item)
+      );
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header countCartItems={cartItems.length}/>
+    <div className="row">
+      <Main onAdd={onAdd} products={products}/>
+      <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems}/>
+    </div>
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            name: "Macbook",
+            Price: 1200,
+            image: 'https://picsum.photos/id/180/2400/1600' 
+        },
+        {
+            id: 2,
+            name: "Car",
+            Price: 1500,
+            image: 'https://picsum.photos/id/111/4400/2656' 
+        },
+        {
+            id: 3,
+            name: "Shoes",
+            Price: 500,
+            image: 'https://picsum.photos/id/21/3008/2008' 
+        }
+    ]
+}
+export default data;
